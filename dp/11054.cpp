d993af83ea20b171bef35c1c3093dfb28340f173
SH-MooDy <shlee4167@naver.com>
2025-01-07 17:32:03 +0900

    임시저장

---
+++ dp/11054.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int A[1000];
+  int dp[1000];
+  int N;
+
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+
+  for (int i = 1; i < N; i++) {
+    int maxNum = 0;
+    for (int j = 0; j < i; j++) {
+    }
+  }
+  return 0;
+}
