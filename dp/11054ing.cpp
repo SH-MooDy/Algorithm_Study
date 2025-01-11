cdb13f1a129f4e2dfbbd88628cd20a519f5c33dd
SH-MooDy <shlee4167@naver.com>
2025-01-11 13:27:29 +0900

    add 11054

--- dp/11054ing.cpp
+++ dp/11054ing.cpp
@@ -10,12 +10,23 @@ int main() {
   cin >> N;
   for (int i = 0; i < N; i++) {
     cin >> A[i];
+    dp[i] = 1;
   }
 
   for (int i = 1; i < N; i++) {
     int maxNum = 0;
     for (int j = 0; j < i; j++) {
+      if (A[j] < A[i]) {
+        maxNum = max(maxNum, dp[j]);
+      }
     }
   }
+
+  int result = 0;
+  for (int i = 0; i < N; i++) {
+    result = max(result, dp[i]);
+  }
+
+  cout << result << endl;
   return 0;
 }
